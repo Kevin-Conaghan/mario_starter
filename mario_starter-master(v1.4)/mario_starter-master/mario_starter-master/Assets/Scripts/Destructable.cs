0940711d37b9d82243ca97fa27222e7fa6afd894
Unknown <kevin_conaghan@hotmail.co.uk>
2017-02-23 10:43:21 +0000

    Kevin's Changes
    Added: Destrucable blocks; Mario death by fall; Mario music

---
+++ mario_starter-master(v1.4)/mario_starter-master/mario_starter-master/Assets/Scripts/Destructable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructable : MonoBehaviour
+{
+    // if player touches block the block will be destroyed
+    void OnControllerColliderHit(ControllerColliderHit other)
+    {
+        if (other.collider.gameObject.tag == "PlatformBlock")
+        {
+            Destroy(other.gameObject);    
+            
+        }
+        
+    }
+   
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        
+	}
+}
